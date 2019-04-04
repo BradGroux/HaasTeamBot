b2b441d6aff94802a821e767ae482226028c0aac
Julian Gindi <julian@gindi.io>
2019-04-03 17:13:31 -0700

    switching test to australian grand prix, which is simply a better test

--- tests/test_data.py
+++ tests/test_data.py
@@ -6,13 +6,13 @@ from .context import haasbot
 import haasbot.data as d
 
 
-@freeze_time("2019-04-03")
+@freeze_time("2019-03-03")
 class DataFunctionTests(unittest.TestCase):
     """Basic data functionality test cases."""
 
     def test_get_next_race(self):
         next_race = d.get_next_race()
-        expected_race_name = "Chinese Grand Prix"
+        expected_race_name = "Australian Grand Prix"
         self.assertEqual(expected_race_name, next_race["raceName"])
 
 
